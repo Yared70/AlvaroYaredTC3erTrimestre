6afe65dcde5decca0ab158072a9ea3d2ddeeaa80
alvarohdr <ivhalvarohdez@gmail.com>
2022-04-04 22:06:52 +0100

    Se crea una primera versión gráfica en la que se muestra por consola la posición del grid que se está pulsando

--- HundirFlota/src/es/iespuertodelacruz/ay/controller/FXMLDocumentController.java
+++ HundirFlota/src/es/iespuertodelacruz/ay/controller/FXMLDocumentController.java
@@ -9,8 +9,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -20,16 +29,32 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private GridPane panel;
+    @FXML
+    private TextArea pruebas;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    private void seleccionarCasilla(MouseEvent event) {
+        /*Button boton = (Button)event.getSource();
+        int num = Integer.parseInt(boton.getText());
+        pruebas.appendText("" + num + "\n");
+        System.out.println(boton.getText());*/
+        
+    }
+
+    @FXML
+    private void seleccionarCasilla(ActionEvent event) {
+        Button boton = (Button)event.getSource();
+        //System.out.println(boton.getLayoutX());
+        //System.out.println(GridPane.getRowIndex(boton) + ", " + GridPane.getColumnIndex(boton));
+        int x = (GridPane.getRowIndex(boton) == null) ? 0:GridPane.getRowIndex (boton);
+        int y = (GridPane.getColumnIndex(boton) == null) ? 0:GridPane.getColumnIndex (boton);
+        System.out.println(x + ", " + y);
+    }
     
 }
